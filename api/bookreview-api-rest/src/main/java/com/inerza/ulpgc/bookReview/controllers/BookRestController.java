28a5d987b19efec18882c22da5137ceb7aef6a9e
Oscar Love <olabradalove@gmail.com>
2023-02-20 08:10:45 +0000

    Added REST functionality to Book

--- api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/controllers/BookRestController.java
+++ api/bookreview-api-rest/src/main/java/com/inerza/ulpgc/bookReview/controllers/BookRestController.java
@@ -11,9 +11,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Tag(description = "Books to be reviewed.", name = "Book Resource")
@@ -43,5 +45,65 @@ public class BookRestController {
         return books.stream()
           .map(x -> BookMapper.INSTANCE.convertToDto(x))
           .collect(Collectors.toList());
+    }
+
+    @Operation(summary = "Create a book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public BookDTO createBook(@RequestBody BookDTO bookDTO) {
+        Book book = BookMapper.INSTANCE.convertToEntity(bookDTO);
+        Book bookCreated = bookService.createBook(book);
+        return BookMapper.INSTANCE.convertToDto(bookCreated);
+    }
+
+    @Operation(summary = "Get a book by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @GetMapping(value = "/{id}")
+    @ResponseBody
+    public BookDTO getBook(@PathVariable("id") Long id) {
+        return BookMapper.INSTANCE.convertToDto(bookService.getBookById(id));
+    }
+
+    @Operation(summary = "Update a book")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateBook(@PathVariable("id") Long id, @RequestBody BookDTO bookDTO) {
+        if (!Objects.equals(id, bookDTO.getId())) {
+            throw new IllegalArgumentException("IDs don't match");
+        }
+
+        Book book = BookMapper.INSTANCE.convertToEntity(bookDTO);
+        bookService.updateBook(book);
+    }
+
+    @Operation(summary = "Delete a book by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "${api.response-codes.ok.desc}"),
+            @ApiResponse(responseCode = "400", description = "${api.response-codes.badRequest.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }),
+            @ApiResponse(responseCode = "404", description = "${api.response-codes.notFound.desc}",
+                    content = { @Content(examples = { @ExampleObject(value = "") }) }) })
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBook(@PathVariable("id") Long id) {
+        bookService.deleteBook(id);
     }
 }
