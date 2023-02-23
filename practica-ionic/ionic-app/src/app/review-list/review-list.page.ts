b8b5cf0736188ef50ae8b36dd9b3db3c9e1a041a
Oscar Love <olabradalove@gmail.com>
2023-02-23 17:57:23 +0000

    Generate page review-list

---
+++ practica-ionic/ionic-app/src/app/review-list/review-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-review-list',
+  templateUrl: './review-list.page.html',
+  styleUrls: ['./review-list.page.scss'],
+})
+export class ReviewListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
